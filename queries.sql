7dacb4a804cdfa51d45b9a8e16852963e0aff5ca
alexfont <alexfont321@gmail.com>
2018-10-11 15:59:21 -0500

    going home

--- queries.sql
+++ queries.sql
@@ -22,3 +22,33 @@ join Album al on al.AlbumId = s.AlbumId
 join Artist ar on ar.ArtistId = s.ArtistId
 join Genre g on g.GenreId = s.GenreId
 ;
+
+
+
+insert into artist 
+(ArtistName, YearEstablished)
+values 
+("Parquet Courts", 2013)
+;
+
+
+
+
+select * from Artist;
+
+--delete from Artist where ArtistId = 36
+;
+
+update Artist 
+	set ArtistName = "Jay-Z"
+	where ArtistId = 27
+	;
+	
+insert into Album
+(Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId)
+values 
+("Light Up Gold", 2013, 2000, "Matador", 37, 2)
+;
+
+select * from Album;
+
